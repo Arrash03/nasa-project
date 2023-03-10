4237aeadba6316615c15ad3e5dd3a369e04e5bf3
Arrash Tahery <atv1382@gmail.com>
2023-03-10 18:19:45 +0330

    sending_data_function

--- back/src/express/app.js
+++ back/src/express/app.js
@@ -1,5 +1,12 @@
 const express = require("express");
+const cors = require("cors");
+const planetsRouter = require("../router/planets/planets.router");
 
 const app = express();
+
+app.use(cors({
+  origin:"http://localhost:3000"
+}));
+app.use(planetsRouter);
 
 module.exports = app;
